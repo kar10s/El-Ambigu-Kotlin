188cb55f9f6345b9a35c711e86f60efd7a1d00d7
kar10s <dknofler@yahoo.com>
2023-09-22 03:05:01 +0200

    initial commit

--- app/src/main/java/org/carlosrp/elambigukotlin/ui/home/MainActivity.kt
+++ app/src/main/java/org/carlosrp/elambigukotlin/ui/home/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.carlosrp.elambigukotlin.R
@@ -13,25 +16,37 @@ import org.carlosrp.elambigukotlin.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController   //Clase padre que controla toda la navegacion
-
+    lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initUI()
-
     }
 
     private fun initUI() {
         initNavigation()
-
+        initAppBarConfiguration()
     }
 
     private fun initNavigation() {
         val navHost =
-            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+            supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment //frg Container
         navController = navHost.navController
+
+        binding.drawerNavView.setupWithNavController(navController)
         binding.bottomNavView.setupWithNavController(navController)
+    }
+
+    private fun initAppBarConfiguration() {
+        setSupportActionBar(binding.toolbar)
+        appBarConfiguration = AppBarConfiguration(navController.graph, binding.layoutDrawer)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
